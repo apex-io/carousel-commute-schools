8db588b447f2f491b464d94c530c9e65cabba7a6
richard-c-gill <richardcarsongill@gmail.com>
2020-03-19 17:27:51 -0700

    set full testing configs

---
+++ spec/react.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {mount, shallow} from 'enzyme'
+
+function Fixture() {
+  return (
+    <div>
+      <input id="checked" defaultChecked />
+      <input id="not" defaultChecked={false} />
+      <input id="tertiary" defaultChecked checked={false} />
+    </div>
+  );
+}
+
+describe('<Fixture />', () => {
+    it('renders three components', () => {
+      const wrapper = mount(<Fixture />);
+      expect(wrapper.find('#checked')).toBeChecked();
+      expect(wrapper.find('#not')).not.toBeChecked();
+    });
+});
+
