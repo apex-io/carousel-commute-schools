6d78b1cf10e43136d9f553a6cb803fc83b753b60
richard-c-gill <richardcarsongill@gmail.com>
2020-03-23 19:04:31 -0700

    changed grid structure

--- src/components/arrow.jsx
+++ src/components/arrow.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
+import { FaChevronCircleRight, FaChevronCircleLeft } from 'react-icons/fa';
+import styled from 'styled-components'
 
-const Arrow = ({ direction, clickFunction, glyph }) => {
-  
+const Chevron = styled.div`
+
+`
+
+
+export const Arrow1 = ({ direction, clickFunction, glyph }) => {
+  const styles = {
+    position: 'absolute',
+    top: '350px',
+    left: '10px'
+  }
   return (
-    <div
+    <div style={styles}
       className={ `slide-arrow ${direction}` }
-      onClick={ clickFunction }>
-      { glyph }
+      onClick={ clickFunction }><FaChevronCircleLeft size={40}/>
     </div>
   );
 }
 
-export default Arrow
+export const Arrow2 = ({ direction, clickFunction, glyph }) => {
+  const styles = {
+    position: 'absolute',
+    top: '350px',
+    right: '10px'
+  }
+  return (
+    <div style={styles}
+      className={ `slide-arrow ${direction}` }
+      onClick={ clickFunction }><FaChevronCircleRight size={40}/>
+    </div>
+  );
+}
+
+
--- src/components/stylePage.jsx
+++ src/components/stylePage.jsx
@@ -4,43 +4,44 @@ import px2vw from "./px2vw.jsx";
 export const Container = styled.div`
   display: flex;
   flex-wrap: nowrap;
-  justify-content: center;
-  margin: ${px2vw(32)};
+  margin: ${px2vw(10)};
   max-width: 100%;
+  justify-content: center;
+
 
   @media (min-width: 500px) {
     flex-wrap: wrap;
   }
 `;
 
-export const Box = styled.div`
+export const Box = styled.img`
   display: flex;
   width: ${px2vw(320, 320)};
   min-height: ${px2vw(200, 320)};
   flex-direction: column;
-  padding: ${px2vw(20)};
-  margin: ${px2vw(20)};
+  margin: ${px2vw(5)};
   height: 100%;
 
-  @media (min-width: 768px) {
+  @media (min-width: 8px) {
     width: ${px2vw(320, 768)};
     min-height: ${px2vw(200, 768)};
     height: 100%;
   }
 
-  @media (min-width: 1024px) {
+  @media (min-width: 4px) {
     width: ${px2vw(500)};
     min-height: ${px2vw(300)};
     height: 100%;
   }
 `;
 
-export const BoxTitle = styled.img`
-  background-image: ${props => `url(${props.image})`};
-
-  @media (min-width: 1024px) {
-    font-size: 1.5rem;
-  }
+export const TopImage = styled.img`
+display: flex;
+  flex-direction: column;
+  height: 100%;
+  align-content: center;
+  width: ${px2vw(320, 500)};
+  
 `;
 
 export const BoxText = styled.p`
@@ -50,5 +51,17 @@ export const BoxText = styled.p`
 
   @media (min-width: 1024px) {
     font-size: 1rem;
+  }
+`;
+
+export const TopImageContainer = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+  
+
+
+  @media (min-width: 500px) {
+    flex-wrap: wrap;
   }
 `;
