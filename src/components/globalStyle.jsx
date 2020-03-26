112f74f4e67c4a8d0958c2df880c0440e607cd85
Dennis Huynh <57211026+dhkaneda@users.noreply.github.com>
2020-03-26 09:45:00 -0700

    Remove unnecessary global styling
    Was conflicting with rest of group modules

--- src/components/globalStyle.jsx
+++ src/components/globalStyle.jsx
@@ -2,12 +2,7 @@ import { createGlobalStyle } from "styled-components";
 import px2vw from "./px2vw.jsx";
 
 export const Global = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  :root {
+  *:root {
       font-size: ${px2vw(24)};
 
       @media (min-width: 768px) {
@@ -17,7 +12,7 @@ export const Global = createGlobalStyle`
       @media (min-width: 1024px) {
         font-size: ${px2vw(16)};
       }
-    }
+   }
 `;
 
 export default Global;
